9eac922e09663e319535eac3978c085ef6ceb4ce
David-Mdrs <medeiros.david.ac@gmail.com>
2024-11-03 17:26:23 -0300

    Função - NumeroPrimoVector

--- ex08.cpp
+++ ex08.cpp
@@ -1,27 +1,52 @@
 #include <iostream>
 #include <array>
+#include <vector>
 
 // Escreva um função que determina se um array-C++ de inteiros recebido como referência contém algum número primo.
+// Escreva um função que determina se um vector recebido como referência contém algum número primo.
 
-void NumeroPrimo(const std::array<int, 5>& arr){
-    for(int i = 0; i < arr.size(); i++){
-        for(int j = 2; j < i-1; j++){
-            if(arr[i] / j == 0){
-                std::cout << "Array possui números primos!" <<std::endl;
-                return;
+void NumeroPrimo(const std::array<int, 5> &arr){
+    for (size_t i = 0; i < arr.size(); i++){
+        if (arr[i] <= 1) continue;
+        bool primo = true;
+        for (int j = 2; j < arr[i]; j++){
+            if (arr[i] % j == 0){
+                primo = false;
+                break;
             }
         }
+        if(primo){
+            std::cout << "O array possui números primos!" << std::endl;
+            return;
+        }
     }
-    std::cout << "Array não possui números primos!" << std::endl;
-    return;
+    std::cout << "O array não possui números primos!" << std::endl;
+}
+
+void NumeroPrimoVector(const std::vector<int>& vec){
+    for (size_t i = 0; i < vec.size(); i++){
+        if (vec[i] <= 1) continue;
+        bool primo = true;
+        for (int j = 2; j < vec[i]; j++){
+            if (vec[i] % j == 0){
+                primo = false;
+                break;
+            }
+        }
+        if(primo){
+            std::cout << "O vetor possui números primos!" << std::endl;
+            return;
+        }
+    }
+    std::cout << "O vetor não possui números primos!" << std::endl;
 }
 
 int main(){
 
-    std::array<int, 5> arr{4, 6, 8, 9, 10};
-    std::array<int, 5> arrPrimo{2, 3, 7, 11};
-    NumeroPrimo(arr);
-    NumeroPrimo(arrPrimo);
+    std::vector<int> vec{4, 6, 8, 9, 10};
+    std::vector<int> vecPrimo{2, 3, 7, 11};
+    NumeroPrimoVector(vec);
+    NumeroPrimoVector(vecPrimo);
 
    return 0;
 }
